445b8222f5a0f36627bb70da2505bb23d16606e5
zm00622 <zarymanning@gmail.com>
2020-02-18 16:55:43 -0500

    Updates

---
+++ precache-manifest.648d9d5eef570bcd8bc5eecc730613e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0afa92ea54d0370efb613b05eae8dd2",
+    "url": "/canyon-theme/index.html"
+  },
+  {
+    "revision": "ad066b03affbe2124140",
+    "url": "/canyon-theme/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa06c83f6f7be6722e92",
+    "url": "/canyon-theme/static/js/2.3dbc2d4a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/canyon-theme/static/js/2.3dbc2d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad066b03affbe2124140",
+    "url": "/canyon-theme/static/js/main.565f36a8.chunk.js"
+  },
+  {
+    "revision": "ff6cc93bb0d30cd63b62",
+    "url": "/canyon-theme/static/js/runtime-main.92234e36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/canyon-theme/static/media/logo.5d5d9eef.svg"
+  }
+]);
