d328ae3f2c8870776fefab03f8deeb32599fba16
zm00622 <zarymanning@gmail.com>
2021-03-06 15:25:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/canyon-theme/precache-manifest.1fef405634e22e9ec224025d92bd5f33.js"
+  "/canyon-theme/precache-manifest.07f5bb669a840be514d49524692c06ba.js"
 );
 
 self.addEventListener('message', (event) => {
