ab839a0afa8a965d0eb15c9fa34767b9fe7530eb
zm00622 <zarymanning@gmail.com>
2021-08-15 10:10:33 -0400

    Updates

---
+++ precache-manifest.dffec84e8fc15d82963de979ddd6e477.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9248fffa4c9271408e7bb37d3be466ef",
+    "url": "/canyon-theme/index.html"
+  },
+  {
+    "revision": "269e6823317458af42d6",
+    "url": "/canyon-theme/static/css/main.08e83db1.chunk.css"
+  },
+  {
+    "revision": "0df0698915dafa66a065",
+    "url": "/canyon-theme/static/js/2.f96aa858.chunk.js"
+  },
+  {
+    "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
+    "url": "/canyon-theme/static/js/2.f96aa858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269e6823317458af42d6",
+    "url": "/canyon-theme/static/js/main.a210df77.chunk.js"
+  },
+  {
+    "revision": "ff6cc93bb0d30cd63b62",
+    "url": "/canyon-theme/static/js/runtime-main.92234e36.js"
+  },
+  {
+    "revision": "188fbd6a6114b14d8aba002cfcc6da74",
+    "url": "/canyon-theme/static/media/MyPic.188fbd6a.jpg"
+  },
+  {
+    "revision": "a28873e7c062839cd4e9a724ec3994ec",
+    "url": "/canyon-theme/static/media/ToDoList.a28873e7.png"
+  },
+  {
+    "revision": "a7114051b183e0e5a98bf6ff67a856ad",
+    "url": "/canyon-theme/static/media/advertisment.a7114051.png"
+  },
+  {
+    "revision": "8ddbef55a46735bf236c531fe17821d1",
+    "url": "/canyon-theme/static/media/bookApp.8ddbef55.png"
+  },
+  {
+    "revision": "606f54f5da0c1430a8a70fb7f3563d51",
+    "url": "/canyon-theme/static/media/canyon.606f54f5.png"
+  },
+  {
+    "revision": "af4698da85167d56e782058ea1d08861",
+    "url": "/canyon-theme/static/media/canyon2.af4698da.png"
+  },
+  {
+    "revision": "b96b71b269360c07a15d8409a0e388ff",
+    "url": "/canyon-theme/static/media/login.b96b71b2.png"
+  },
+  {
+    "revision": "ad75661098ac838cb2758a3b7ad4168e",
+    "url": "/canyon-theme/static/media/onlineCourse.ad756610.png"
+  },
+  {
+    "revision": "6eb874d22133c20277d5a5868e7e5f44",
+    "url": "/canyon-theme/static/media/stockApp.6eb874d2.png"
+  },
+  {
+    "revision": "c18b29a9982c4ed30f0e6a8e83bf41ec",
+    "url": "/canyon-theme/static/media/thisSite.c18b29a9.png"
+  },
+  {
+    "revision": "6b9026d7e7592d4b349a8a98630f8b11",
+    "url": "/canyon-theme/static/media/websiteBanner.6b9026d7.png"
+  }
+]);
